d5f9278600a84861269a63a7553852a03e5ab4a5
koderrapi <fauyannn@gmail.com>
2024-04-17 16:29:53 +0700

    send eth

---
+++ scripts/sendEth.js
@@ -0,0 +1,33 @@
+const hre = require("hardhat");
+
+// const SEND_MONEY = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+
+async function main() {
+
+    // create the provider to access arbitrum goerli testnet
+const provider = new ethers.JsonRpcProvider('https://arbitrum-goerli.public.blastapi.io')
+// create wallet using private address and provider
+const wallet = new ethers.Wallet('0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', provider)
+// the receiver
+const recipient = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+const amountInEther = '0.001'
+// transaction data, recipient and value in wei
+const txData = {
+  to: recipient,
+  value: ethers.parseEther(amountInEther)// eth to wei
+}
+console.log(`Sending ${amountInEther}eth to ${recipient}`)
+// send transaction with the wallet
+const tx = await wallet.sendTransaction(txData)
+console.log(`Waiting tx... ${tx.hash}`)
+// wait transaction to confirm at least 1 block
+const finishedTx = await tx.wait()
+console.log(`Tx executed ${finishedTx.hash}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
